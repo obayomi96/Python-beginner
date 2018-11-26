7386e710880e89b932f39e940de08514c6b75442
obayomi96 <martinsoluwaseun47@gmail.com>
2018-11-26 12:23:22 +0000

    Program to calculate and print the multiplication table for any number

--- multiplication.py
+++ multiplication.py
@@ -11,3 +11,5 @@ print("Welcome ", userName)
 value = int(input("Please enter a number: "))
 for i in range(1, 13, 1):
     print(value, "*", i, "=", value*i)
+
+print("You now have your Multiplication table so the program has ended")
